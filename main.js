4587f9cda5f92b14a1107b219a16044f89c7d94a
Barbora Vesela <vesela.bara@gmail.com>
2023-02-01 21:31:37 +0100

    ukol

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
 // tady je místo pro náš program
+
+let text = document.querySelector('.text');
+
+function onHover() {
+    text.classList.toggle("tucne");
+}
+    
+function zmenBarvu() {
+    text.classList.toggle('cervene');
+}
+
+function zmenVelikost() {
+    text.classList.add('vetsi')
+}
+
+
+let tlacitko = document.querySelector('.tlacitko')
+
+function zavolejUdalost(udalost) {
+    let prvek = udalost.target;
+    prvek.style.hidden = 'true';
+    console.log(udalost);
+    console.log(prvek); 
+}
+
+let prehravac = document.querySelector('.mujPrehravac')
+
+function spustAudio() {
+    prehravac.play();
+}
+
+function zastavAudio() {
+    prehravac.pause();
+}
+
+function resetAudio() {
+    prehravac.currentTime = 0;
+}
+
+let currentVolume = 0.5;
+function minAudio() {
+    prehravac.volume = 0;
+}
+
+function origAudio() {
+    prehravac.volume = currentVolume;
+}
+
+function vicAudio() {
+    prehravac.volume = 1;
+}
+
